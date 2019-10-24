90a305d4c50d8d4fcfcf19e2e584349ddd1f96a3
Faisal Aqdas <faisalaqdas@gmail.com>
2019-10-24 23:16:54 +0500

    update result

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,22 @@
 var express = require('express');
 var router = express.Router();
-const fetch = require('node-fetch');
+const axios = require('axios').default;
+const xml2js = require('xml2js-es6-promise');
 /* GET home page. */
-router.get('/',async function(req, res, next) {
+router.get('/', async function (req, res, next) {
 
- try {
- const result = await fetch('https://portal.onairtel.com/xmlapi/xmlapi', {
-    method: 'POST',
-    headers:  {  'Content-Type': 'application/x-www-form-urlencoded'    } ,
-    body: '<?xml version="1.0"?> <methodCall> <methodName>listCustomers</methodName><params><param><value><struct> <member> </member> </struct></value></param> </params> </methodCall>' 
-  });
-  console.log(result)
+  try {
+    const xml = '<?xml version="1.0"?> <methodCall> <methodName>listCustomers</methodName><params><param><value><struct> <member> </member> </struct></value></param> </params> </methodCall>';
+    const xmlResult = await axios.post("https://www.w3schools.com/xml/cd_catalog.xml",xml,{headers: { 'content-type': 'application/x-www-form-urlencoded' },});
+    const result = await xml2js(xmlResult.data);
+    
+    console.log(JSON.stringify(result));
+    res.send(result);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
 
-  res.send({
-    result:result
-  });
- } catch (error) {
-   console.log(error);
-   res.status(400).send(error);
- }
- 
 });
 
 module.exports = router;
